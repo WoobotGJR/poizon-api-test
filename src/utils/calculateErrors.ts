5244ea24d0297e9811d37ce729d1c1f3c6f09baa
Andrey Karagichev <Karagichev2002@gmail.com>
2024-08-12 18:54:17 +0300

    file deleted

--- src/utils/calculateErrors.ts
+++
@@ -1,5 +0,0 @@
-export const calculateErrors = (input: string, text: string) => {
-  return input.split('').reduce((acc, char, index) => {
-    return acc + (char !== text[index] ? 1 : 0);
-  }, 0);
-};
